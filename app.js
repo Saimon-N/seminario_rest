f70e2d8732b6eee7fb4ee111bf009a2607a3a0e6
Nicolas <black.sword785@gmail.com>
2020-07-05 22:23:19 -0400

    conexion ordenes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var restaurantRouter = require('./routes/restaurant');   //---->link para restaurant
 var menuRouter = require('./routes/menu');   //---->link para menu
+var ordenesRouter = require('./routes/ordenes');   //---->link para ordenes
 //var userRouter = require('./routes/user');
 
 var app = express();
@@ -22,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/1.0', restaurantRouter);  //---->link para restaurant
 app.use('/api/1.0', menuRouter);  //---->link para menu
+app.use('/api/1.0', ordenesRouter);  //---->link para ordenes
+
 //app.use('/api/1.0', userRouter);
 
 // catch 404 and forward to error handler
